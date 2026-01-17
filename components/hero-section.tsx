d68f134114a163eb60bfbc63129c0a009616659b
faysel <fayselcode@gmail.com>
2026-01-17 19:43:54 +0300

    fix

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -30,7 +30,7 @@ export default function HeroSection() {
             <h1 className="text-[1.67rem] md:text-[2.67rem] lg:text-[3.35rem]  tracking-tight font-medium font-sf-pro mb-5 md:mb-6 md:leading-[120%] text-white">
               {t("mainHeading")}
             </h1>
-              <h1 className="-mt-10 max-lg:-mt-9 rtl:-mt-6 rtl:max-md:-mt-7 text-[1.67rem] md:text-[2.67rem] lg:text-[3.35rem]  tracking-tight font-medium font-sf-pro mb-5 md:mb-6 md:leading-[120%] text-white">
+            <h1 className="-mt-10 max-lg:-mt-9 rtl:-mt-6 rtl:max-md:-mt-7 text-[1.67rem] md:text-[2.67rem] lg:text-[3.35rem]  tracking-tight font-medium font-sf-pro mb-5 md:mb-6 md:leading-[120%] text-white">
               {t("mainHeading2")}
             </h1>
 
@@ -39,18 +39,8 @@ export default function HeroSection() {
               {t("subHeading")}
             </p>
 
-            {/* CTA Button */}
-            <button
-              className="cursor-pointer inline-block px-7 py-4  rounded-lg font-gilroy text-black md:border-0 transition-all hover:opacity-90 w-fit"
-              style={{ backgroundColor: "#00B3C6" }}
-            >
-              {t.rich("buttonText", {
-                bold: (chunk) => <span className="font-semibold">{chunk}</span>,
-              })}
-            </button>
-
             {/* Feature Box - Desktop only */}
-            <div className="hidden lg:flex   mt-16  max-w-md">
+            <div className="flex  max-w-md">
               <div className=" flex-1 max-w-1 rounded-full bg-white"></div>
               <div className="ps-5">
                 <p className="font-medium mb-2 text-white text-base">
@@ -65,12 +55,24 @@ export default function HeroSection() {
                 </p>
               </div>
             </div>
+
+            {/* CTA Button */}
+            <button
+              className="cursor-pointer inline-block px-7 py-4  rounded-lg font-gilroy text-black md:border-0 transition-all hover:opacity-90 w-fit mt-16 "
+              style={{ backgroundColor: "#00B3C6" }}
+            >
+              {t.rich("buttonText", {
+                bold: (chunk) => <span className="font-semibold">{chunk}</span>,
+              })}
+            </button>
           </div>
 
           {/* Right visual - anchored bottom-right */}
           <div className="lg:relative h-36 lg:h-full w-full lg:ltr:-mr-16  xl:ltr:-mr-32 top-0  lg:top-15 lg:pb-0  max-lg:justify-center max-lg:items-center max-lg:flex max-lg:my-5">
-            <div className="lg:absolute lg:bottom-0 ltr:right-0 rtl:left-0 w-full max-w-44
-             lg:max-w-sm">
+            <div
+              className="lg:absolute lg:bottom-0 ltr:right-0 rtl:left-0 w-full max-w-44
+             lg:max-w-sm"
+            >
               {/* Brain visualization */}
               <div className="relative w-full aspect-square rounded-full overflow-hidden animate-pulse">
                 <Image
@@ -79,15 +81,15 @@ export default function HeroSection() {
                   fill
                   className="object-cover object-bottom opacity-75"
                   priority
-                  quality={100} 
-                  />
+                  quality={100}
+                />
               </div>
             </div>
           </div>
         </div>
 
         {/* Feature Box - Mobile only (below brain) */}
-        <div className="lg:hidden border-s-4 ps-4 border-white">
+        {/* <div className="lg:hidden border-s-4 ps-4 border-white">
           <p className="font-medium mb-2 text-white text-sm">
             {t.rich("featureLabel", {
               highlight: (chunk) => (
@@ -98,7 +100,7 @@ export default function HeroSection() {
           <p className="text-gray-400 text-xs leading-relaxed">
             {t("featureText")}
           </p>
-        </div>
+        </div> */}
       </div>
     </section>
   );
