8f7550175fd444409a787b941e341cf60609d6c6
Abubeker Abduljelil <Abubekera87@gmail.com>
2026-02-09 11:55:25 +0300

    feat: change glass  blur

--- components/glass-card.tsx
+++ components/glass-card.tsx
@@ -18,7 +18,7 @@ const GlassCard = ({
 }: GlassCardsProps) => {
   return (
     <div className="group w-full">
-      <div className="group-hover:translate-y-1 transition-all duration-300 scale-85 blur-xs text-muted-foreground p-6 rounded-2xl bg-card border flex ">
+      <div className="group-hover:translate-y-1 transition-all duration-300 scale-85 blur-sm  text-muted-foreground p-6 rounded-2xl bg-card border flex ">
         <div className="mt-1">{<Icon />}</div>
         <div className="flex flex-col ms-5 w-full">
           <p className="text-xl leading-tight">{title}</p>
