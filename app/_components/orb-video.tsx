7c3c8baa66ce39cfe4fa3dd1cfca0d1765d70f5c
Abubeker Abduljelil <Abubekera87@gmail.com>
2026-02-09 10:21:03 +0300

    feat: fade in orb video

--- app/_components/orb-video.tsx
+++ app/_components/orb-video.tsx
@@ -1,22 +1,32 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import { cn } from "@/lib/utils";
+import React, { useEffect, useRef, useState } from "react";
 
 const OrbVideo = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     if (videoRef.current) {
-      videoRef.current.playbackRate = 0.85;
+      videoRef.current.playbackRate = 0.78;
     }
   }, []);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
-    <div className="relative w-full aspect-square  max-lg:scale-115   overflow-hidden ">
+    <div
+      className={cn(
+        "relative w-full aspect-square  opacity-0 transition duration-500  max-lg:scale-115   overflow-hidden  mask-[radial-gradient(circle_at_center,black_60%,transparent_100%)] [-webkit-mask-image:radial-gradient(circle_at_center,black_60%,transparent_100%)]",
+        loaded && "opacity-100",
+      )}
+    >
       <video
         ref={videoRef}
-        src="/orb_clip_mobile.webm"
-        className="w-full h-full object-cover  
-             mask-[radial-gradient(circle_at_center,black_60%,transparent_100%)]
-             [-webkit-mask-image:radial-gradient(circle_at_center,black_60%,transparent_100%)]"
+        src="/orb_clip-2.webm"
+        className={cn("w-full h-full object-cover ")}
         autoPlay
         muted
         loop
